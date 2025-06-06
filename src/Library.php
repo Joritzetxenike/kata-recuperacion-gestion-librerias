749dae87ce81c2ef4b220a611384ce0abae11195
Joritz <echenique.149324@e.unavarra.es>
2025-06-06 16:30:19 +0200

    [refactor] - Refactor para que la lista de libros sea un array y trabajar con esta

--- src/Library.php
+++ src/Library.php
@@ -4,16 +4,41 @@ namespace Deg540\DockerPHPBoilerplate;
 
 class Library
 {
+    private array $listOfLoanBooks = [];
     public function operations(string $books): string
     {
         $listOfBooks = explode("prestar: ", $books);
+
         if (isset($listOfBooks[1])) {
             $quantityBooks = explode(" ", $listOfBooks[1]);
+
             if (isset($quantityBooks[1])) {
-                return $quantityBooks[0] . " x" . $quantityBooks[1];
+                $this->listOfLoanBooks[$quantityBooks[0]] = intval($quantityBooks[1]);
+                return $this->printLoanBooks();
             }
-            return $listOfBooks[1] . " x1";
+
+            $this->listOfLoanBooks[$quantityBooks[0]] = 1;
+            return $this->printLoanBooks();
         }
-        return "";
+        return $this->printLoanBooks();
+    }
+
+    private function printLoanBooks():string
+    {
+        ksort($this->listOfLoanBooks);
+        return $this->mapped_implode(', ', $this->listOfLoanBooks, ' x');
+
+    }
+
+    private function mapped_implode($glue, $array, $symbol = '=') {
+
+        return implode($glue, array_map(
+                function($k, $v) use($symbol) {
+                    return $k . $symbol . $v;
+                },
+                array_keys($array),
+                array_values($array)
+            )
+        );
     }
 }
