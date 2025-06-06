4ffdc5d31fef4798f158b7b1ef68a865e38cc3cb
Joritz <echenique.149324@e.unavarra.es>
2025-06-06 17:25:43 +0200

    [refactor] - Logica de operaciones pasada a funciones para mejorar legibilidad

--- src/Library.php
+++ src/Library.php
@@ -8,43 +8,15 @@ class Library
     public function operations(string $bookListWithOperation): string
     {
         if(str_contains($bookListWithOperation,"prestar")) {
-            $bookList = explode(" ",$bookListWithOperation);
-
-            if(!isset($bookList[1])) {
-                return $this->printLoanBooks();
-            }
-
-            if(array_key_exists($bookList[1], $this->listOfLoanBooks)) {
-                isset($bookList[2]) ? $this->listOfLoanBooks[$bookList[1]] += intval($bookList[2]) :  $this->listOfLoanBooks[$bookList[1]] += 1;
-                return $this->printLoanBooks();
-
-            }
-
-            isset($bookList[2]) ? $this->listOfLoanBooks[$bookList[1]] = intval($bookList[2]) :  $this->listOfLoanBooks[$bookList[1]] = 1;
-            return $this->printLoanBooks();
+            return $this->addBookToLoanList($bookListWithOperation);
         }
 
         if (str_contains($bookListWithOperation,"limpiar")) {
-            $this->listOfLoanBooks = [];
-            return $this->printLoanBooks();
+            return $this->clearListOfLoans();
         }
 
         if(str_contains($bookListWithOperation,"devolver")) {
-            $bookList = explode(" ",$bookListWithOperation);
-
-            if(!isset($bookList[1])) {
-                return $this->printLoanBooks();
-            }
-
-            if(!array_key_exists($bookList[1], $this->listOfLoanBooks)) {
-                return "El libro indicado no está en préstamo";
-            }
-            if($this->listOfLoanBooks[$bookList[1]] > 1) {
-                $this->listOfLoanBooks[$bookList[1]] -= 1;
-                return $this->printLoanBooks();
-            }
-            unset($this->listOfLoanBooks[$bookList[1]]);
-            return $this->printLoanBooks();
+            return $this->deleteFromLoanList($bookListWithOperation);
         }
         return $this->printLoanBooks();
     }
@@ -66,5 +38,48 @@ class Library
                 array_values($array)
             )
         );
+    }
+
+    private function clearListOfLoans(): string
+    {
+        $this->listOfLoanBooks = [];
+        return $this->printLoanBooks();
+    }
+
+    private function addBookToLoanList(string $bookListWithOperation): string
+    {
+        $bookList = explode(" ",$bookListWithOperation);
+
+        if(!isset($bookList[1])) {
+            return $this->printLoanBooks();
+        }
+
+        if(array_key_exists($bookList[1], $this->listOfLoanBooks)) {
+            isset($bookList[2]) ? $this->listOfLoanBooks[$bookList[1]] += intval($bookList[2]) :  $this->listOfLoanBooks[$bookList[1]] += 1;
+            return $this->printLoanBooks();
+
+        }
+
+        isset($bookList[2]) ? $this->listOfLoanBooks[$bookList[1]] = intval($bookList[2]) :  $this->listOfLoanBooks[$bookList[1]] = 1;
+        return $this->printLoanBooks();
+    }
+
+    private function deleteFromLoanList(string $bookListWithOperation): string
+    {
+        $bookList = explode(" ",$bookListWithOperation);
+
+        if(!isset($bookList[1])) {
+            return $this->printLoanBooks();
+        }
+
+        if(!array_key_exists($bookList[1], $this->listOfLoanBooks)) {
+            return "El libro indicado no está en préstamo";
+        }
+        if($this->listOfLoanBooks[$bookList[1]] > 1) {
+            $this->listOfLoanBooks[$bookList[1]] -= 1;
+            return $this->printLoanBooks();
+        }
+        unset($this->listOfLoanBooks[$bookList[1]]);
+        return $this->printLoanBooks();
     }
 }
